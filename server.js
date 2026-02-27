a7f855e54840507d80006752be272e6815704dc1
Guren <glenn.zaballero@fligno.com>
2026-02-27 10:19:15 +0800

    Fix Render port binding

--- server.js
+++ server.js
@@ -187,6 +187,8 @@ app.post("/api/validate", async (req, res) => {
   }
 });
 
-app.listen(3001, "127.0.0.1", () => {
-  console.log("Server running on http://127.0.0.1:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
